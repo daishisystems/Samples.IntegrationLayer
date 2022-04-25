e8fc8c8fc38e0ea9ec17c0f95a017b490cf175e7
Paul Mooney <paul@gmst.ie>
2022-04-25 13:48:54 +0100

    Added medications feature

---
+++ Controllers/MedicationsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Samples.IntegrationLayer.Models;
+
+namespace Samples.IntegrationLayer.Controllers
+{
+    public class MedicationsController : Controller
+    {
+        private readonly MedicationDBContext db = new MedicationDBContext();
+
+        // GET: Medications
+        public ActionResult Index()
+        {
+            return View(db.Medications.ToList());
+        }
+
+        // GET: Medications/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var medication = db.Medications.Find(id);
+            if (medication == null) return HttpNotFound();
+            return View(medication);
+        }
+
+        // GET: Medications/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MedicationId,MemberId,Name,Expiry")] Medication medication)
+        {
+            if (ModelState.IsValid)
+            {
+                medication.MedicationId = Guid.NewGuid();
+                db.Medications.Add(medication);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medication);
+        }
+
+        // GET: Medications/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var medication = db.Medications.Find(id);
+            if (medication == null) return HttpNotFound();
+            return View(medication);
+        }
+
+        // POST: Medications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MedicationId,MemberId,Name,Expiry")] Medication medication)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medication).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medication);
+        }
+
+        // GET: Medications/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var medication = db.Medications.Find(id);
+            if (medication == null) return HttpNotFound();
+            return View(medication);
+        }
+
+        // POST: Medications/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            var medication = db.Medications.Find(id);
+            db.Medications.Remove(medication);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Medication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Samples.IntegrationLayer.Models
+{
+    public class Medication
+    {
+        public Guid MedicationId { get; set; }
+        public Guid MemberId { get; set; }
+        public string Name { get; set; }
+        public string Expiry { get; set; }
+    }
+
+    public class MedicationDBContext : DbContext
+    {
+        public DbSet<Medication> Medications { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("treatment");
+        }
+    }
+}
